c2eb54f61a2fb652fc0cb3ec65449d644eb4ba3a
Pete Bunting <petebunting@mac.com>
2023-03-24 16:06:32 +0000

    Add script to create LUT for the dem images.

---
+++ 05_calc_elev_thresholds/09_create_cop30_dem_lut.py
@@ -0,0 +1,9 @@
+import glob
+import rsgislib.imageutils.imagelut
+
+input_imgs = glob.glob("/bigdata/petebunting/GlobalMangroveWatch/gmw_hab_extent/data/cop30_extract_tiles/*/DEM/*.tif")
+
+vec_file = "/bigdata/petebunting/GlobalMangroveWatch/gmw_hab_extent/data/cop30_scns_lut.gpkg"
+vec_lyr = "cop30_scns_lut"
+
+rsgislib.imageutils.imagelut.create_img_extent_lut(input_imgs, vec_file, vec_lyr, out_format="GPKG", ignore_none_imgs = False, out_proj_wgs84 = False, overwrite_lut_file = True)
