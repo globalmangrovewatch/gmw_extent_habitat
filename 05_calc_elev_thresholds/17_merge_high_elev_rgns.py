3a5b51c69003e966fe33bc8c7f40144dcee9f57a
Pete Bunting <petebunting@mac.com>
2023-03-27 09:30:46 +0100

    Add script to merge high elevation regions.

---
+++ 05_calc_elev_thresholds/17_merge_high_elev_rgns.py
@@ -0,0 +1,13 @@
+import glob
+import rsgislib.vectorutils
+
+vec_files = glob.glob(
+    "/bigdata/petebunting/GlobalMangroveWatch/gmw_hab_extent/data/gmw_tile_v3_ext_high_elev/*.gpkg"
+)
+
+rsgislib.vectorutils.merge_vector_files(
+    vec_files,
+    out_vec_file="gmw_v3_2015_high_elev_rgns.gpkg",
+    out_vec_lyr="gmw_v3_2015_high_elev_rgns",
+    out_format="GPKG",
+)
