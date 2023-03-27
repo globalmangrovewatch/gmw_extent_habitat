6cd0e5fadf404d78238f98629ec21865eb28f36c
Pete Bunting <petebunting@mac.com>
2023-03-27 15:20:59 +0100

    Updated.

--- 05_calc_elev_thresholds/17_merge_high_elev_rgns.py
+++ 05_calc_elev_thresholds/17_merge_high_elev_rgns.py
@@ -2,12 +2,12 @@ import glob
 import rsgislib.vectorutils
 
 vec_files = glob.glob(
-    "/bigdata/petebunting/GlobalMangroveWatch/gmw_hab_extent/data/gmw_tile_v3_ext_high_elev/*.gpkg"
+    "/bigdata/petebunting/GlobalMangroveWatch/gmw_hab_extent/data/gmw_tile_v3_v4_002_inter_ext_high_elev/*.gpkg"
 )
 
 rsgislib.vectorutils.merge_vector_files(
     vec_files,
-    out_vec_file="gmw_v3_2015_high_elev_rgns.gpkg",
-    out_vec_lyr="gmw_v3_2015_high_elev_rgns",
+    out_vec_file="gmw_v3_v4_002_inter_high_elev_rgns.gpkg",
+    out_vec_lyr="gmw_v3_v4_002_inter_high_elev_rgns",
     out_format="GPKG",
 )
