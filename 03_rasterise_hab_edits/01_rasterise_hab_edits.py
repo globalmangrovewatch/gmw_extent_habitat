8842df05da256a464bbdd8fc07f2597f9c98d738
Pete Bunting <petebunting@mac.com>
2023-08-09 09:08:56 +0100

    Updated script to rasterise habitat edits.

--- 03_rasterise_hab_edits/01_rasterise_hab_edits.py
+++ 03_rasterise_hab_edits/01_rasterise_hab_edits.py
@@ -38,13 +38,13 @@ out_rm_dir = "gmw_hab_rms"
 if not os.path.exists(out_rm_dir):
     os.mkdir(out_rm_dir)
 
-gmw_hab_add_vec_file = "../00_edits/edits_v12_v13/GMW_Hab_v12_add.geojson"
-gmw_hab_add_vec_lyr = "GMW_Hab_v12_add"
+gmw_hab_add_vec_file = "../00_edits/edits_v13_v14/GMW_Hab_v13_add.geojson"
+gmw_hab_add_vec_lyr = "GMW_Hab_v13_add"
 
-gmw_hab_rm_vec_file = "../00_edits/edits_v12_v13/GMW_Hab_v12_rm.geojson"
-gmw_hab_rm_vec_lyr = "GMW_Hab_v12_rm"
+gmw_hab_rm_vec_file = "../00_edits/edits_v13_v14/GMW_Hab_v13_rm.geojson"
+gmw_hab_rm_vec_lyr = "GMW_Hab_v13_rm"
 
-gmw_hab_version = "v12_to_v13"
+gmw_hab_version = "v13_to_v14"
 
 add_vec_ds_obj, add_vec_lyr_obj = rsgislib.vectorutils.read_vec_lyr_to_mem(
     vec_file=gmw_hab_add_vec_file, vec_lyr=gmw_hab_add_vec_lyr
