05b41599c2904ce0304bfaa57aa60713b2d707a1
Pete Bunting <petebunting@mac.com>
2023-08-09 15:32:39 +0100

    Updated script to vectorise mask.

--- 07_vectorise_hab/02_merge_vec_tiles.py
+++ 07_vectorise_hab/02_merge_vec_tiles.py
@@ -4,8 +4,8 @@ import rsgislib.vectorutils
 
 vec_tiles = glob.glob("gmw_hab_vec_tiles/*.gpkg")
 
-out_vec_file = "gmw_hab_msk_v13.gpkg"
-out_vec_lyr = "gmw_hab_msk_v13"
+out_vec_file = "gmw_hab_msk_v14.gpkg"
+out_vec_lyr = "gmw_hab_msk_v14"
 
 rsgislib.vectorutils.merge_vector_files(
     vec_tiles, out_vec_file, out_vec_lyr, out_format="GPKG", out_epsg=4326
