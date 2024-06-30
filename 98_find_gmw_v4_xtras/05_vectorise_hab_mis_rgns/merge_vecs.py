d7587d5ccce9d0fcc98694bc8ae106f05ae93282
Pete Bunting <petebunting@mac.com>
2024-06-30 16:15:04 +0100

    Added scripts for creating vecs.

---
+++ 98_find_gmw_v4_xtras/05_vectorise_hab_mis_rgns/merge_vecs.py
@@ -0,0 +1,15 @@
+import rsgislib.vectorutils
+import glob
+
+vec_files = glob.glob("/bigdata/petebunting/GlobalMangroveWatch/gmw_hab_extent/data/gmw_v4019_mis_hab_tiles_vec/*.gpkg")
+
+out_vec_file = "gmw_v4019_hab_msk_mis.gpkg"
+out_vec_lyr = "gmw_v4019_hab_msk_mis"
+
+rsgislib.vectorutils.merge_vector_files(
+    vec_files,
+    out_vec_file=out_vec_file,
+    out_vec_lyr=out_vec_lyr,
+    out_format = "GPKG",
+    out_epsg = 4326,
+)
