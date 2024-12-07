2dea8fdeb0640c9a54d6e3829a27b55817765e7d
Pete Bunting <petebunting@mac.com>
2024-12-07 21:19:30 +0000

    Added vectorise habitat diff mask

---
+++ 08_find_hab_ver_diffs/02_vectorise_hab_adds/merge_vec_files.py
@@ -0,0 +1,15 @@
+import rsgislib.vectorutils
+import glob
+
+vec_files = glob.glob("gmw_hab_diff_vec_tiles/*.gpkg")
+
+out_vec_file = "gmw_hab_msk_diff_v20_v22.gpkg"
+out_vec_lyr = "gmw_hab_msk_diff_v20_v22"
+
+rsgislib.vectorutils.merge_vector_files(
+    vec_files,
+    out_vec_file=out_vec_file,
+    out_vec_lyr=out_vec_lyr,
+    out_format = "GPKG",
+    out_epsg = 4326,
+)
