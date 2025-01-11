e2a660424f86c915e5bfc1975cc92aafcae0762a
Pete Bunting <petebunting@mac.com>
2025-01-11 16:58:09 +0000

    Updated.

--- 03_rasterise_hab_edits/run_checks.sh
+++ 03_rasterise_hab_edits/run_checks.sh
@@ -1,2 +1,2 @@
-rsgischkgdalfile.py -i "./gmw_hab_adds/*.tif" --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --printerrs
+rsgischkgdalfile.py -i "./gmw_hab_adds/*.kea" --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --printerrs --chksum
 #rsgischkgdalfile.py -i "./gmw_hab_rms/*.tif" --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --printerrs
