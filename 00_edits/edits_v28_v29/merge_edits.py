145c67c898b7287e1fe0746009daa8cbb5b35c47
Pete Bunting <petebunting@mac.com>
2026-01-14 11:47:19 +0000

    Updated.

--- 00_edits/edits_v28_v29/merge_edits.py
+++ 00_edits/edits_v28_v29/merge_edits.py
@@ -1,3 +1,3 @@
 import rsgislib.vectorutils
 
-rsgislib.vectorutils.merge_vector_files(vec_files = ["/Users/pete/Library/CloudStorage/Dropbox/University/Research/Data/Mangroves/GMW_Habitat_Layers/habitat_edits/v28_v29/gmw_hab_v28_add.geojson"], out_vec_file = "gmw_v28_hab_add.geojson", out_vec_lyr = "gmw_v28_hab_add", out_format = 'GeoJSON', out_epsg = None, remove_cols = None)
+rsgislib.vectorutils.merge_vector_files(vec_files = ["/Users/pete/Library/CloudStorage/Dropbox/University/Research/Data/Mangroves/GMW_Habitat_Layers/habitat_edits/v28_v29/gmw_hab_v28_add.geojson"], out_vec_file = "gmw_v29_hab_add.geojson", out_vec_lyr = "gmw_v29_hab_add", out_format = 'GeoJSON', out_epsg = None, remove_cols = None)
