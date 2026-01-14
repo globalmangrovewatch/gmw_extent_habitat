354e21747fe18c2c4e55f26bf55c9a7fc207065e
Pete Bunting <petebunting@mac.com>
2026-01-14 12:37:51 +0000

    Updatd.

--- 08_find_hab_ver_diffs/02_vectorise_hab_adds/merge_vec_files.py
+++ 08_find_hab_ver_diffs/02_vectorise_hab_adds/merge_vec_files.py
@@ -3,8 +3,8 @@ import glob
 
 vec_files = glob.glob("gmw_hab_diff_vec_tiles/*.gpkg")
 
-out_vec_file = "gmw_hab_msk_diff_v28_v98.gpkg"
-out_vec_lyr = "gmw_hab_msk_diff_v28_v98"
+out_vec_file = "gmw_hab_msk_diff_v28_v29.gpkg"
+out_vec_lyr = "gmw_hab_msk_diff_v28_v29"
 
 rsgislib.vectorutils.merge_vector_files(
     vec_files,
